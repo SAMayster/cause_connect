abb60de0e94d55995eaad5da40ef1384c10a0725
Samay Kochar <samay.kochar@cognizant.com>
2024-03-18 16:23:22 +0530

    some errors

--- cause_c/src/main/java/com/cause_connect/cause_c/controller/DonationController.java
+++ cause_c/src/main/java/com/cause_connect/cause_c/controller/DonationController.java
@@ -3,11 +3,16 @@ package com.cause_connect.cause_c.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.cause_connect.cause_c.model.Cause;
 import com.cause_connect.cause_c.model.Donation;
@@ -17,36 +22,50 @@ import com.cause_connect.cause_c.service.DonationService;
 import jakarta.servlet.http.HttpServletRequest;
 
 
-@RestController
-@RequestMapping("")
+@Controller
 public class DonationController {
 
     @Autowired
     private DonationService donationservice;
 
     
+    @GetMapping("/donate")
+    public String showDonationForm(Model model) {
+    model.addAttribute("donation", new Donation());
+    return "donate"; // Name of the Thymeleaf view for the donation form
+}
 
-    @PostMapping("/donate")
-    public String addDonation(@ModelAttribute Donation donation, HttpServletRequest request, Model model) {
-        // Get the user from the session
-        User user = (User) request.getSession().getAttribute("user");
-        System.out.println("User in session: " + user);
-        if (user == null) {
-            // If the user is not logged in, redirect to the login page
-            model.addAttribute("message", "Please log in to make a donation");
-            return "redirect:/login"; // Assuming "/login" is your login endpoint
+@PostMapping("/donate")
+public ModelAndView addDonation(@RequestParam String causeName, @RequestParam String userId, @ModelAttribute Donation donation, HttpServletRequest request) {
+    // Get the user from the session
+    User user = (User) request.getSession().getAttribute("user");
+    System.out.println("User in session: " + user);
+
+    ModelAndView modelAndView = new ModelAndView();
+
+    if (user == null) {
+        // If the user is not logged in, redirect to the login page
+        modelAndView.addObject("message", "Please log in to make a donation");
+        modelAndView.setViewName("redirect:/login");
+    } else {
+        // If the user is logged in, proceed with the donation
+        donation.setUser(user);
+        ResponseEntity<?> response = donationservice.addDonation(causeName, donation, Integer.parseInt(userId));
+
+        if (response.getStatusCode() == HttpStatus.CREATED) {
+            modelAndView.addObject("message", "Thank you for your donation!");
         } else {
-            // If the user is logged in, proceed with the donation
-            donation.setUser(user); // Set the user in the Donation object
-            ResponseEntity<?> response = donationservice.addDonation(donation.getCause().getName(), donation, user.getUid());
-            if (response.getStatusCode() == HttpStatus.CREATED) {
-                model.addAttribute("message", "Thank you for your donation!");
-            } else {
-                model.addAttribute("message", response.getBody().toString());
-            }
-            return "donationStatus"; // Name of the Thymeleaf view
+            modelAndView.addObject("message", response.getBody().toString());
         }
+
+        modelAndView.setViewName("donationstatus");
     }
+
+    return modelAndView;
+}
+
+
+
     
 //     @PostMapping("/donate")
 // public String addDonation(@ModelAttribute Donation donation, HttpServletRequest request, Model model) {
